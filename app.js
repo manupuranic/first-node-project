f1548aacc837c2918665a8a5bc9ea4f85409a645
manupuranic <manupuranic@gmail.com>
2023-07-01 18:56:24 +0530

    send html response based on url

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Manu");
+  res.setHeader("content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node App</title></head>");
+  if (req.url == "/home") {
+    res.write("<body><h1>Welcome home</h1></body>");
+  } else if (req.url === "/about") {
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+  } else if (req.url === "/node") {
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+  }
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(4000);
